9f3f08777e177c363d25a0ff8011b2c227a1a3e2
deruip <aronly1997@hotmail.com>
2017-09-15 02:10:05 -0400

    Created Lab2
    Pixel art of myself + changing colour of shirt by pressing buttons

---
+++ COMP305-F2017-Lab2/Assets/Scripts/SpriteChanger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteChanger : MonoBehaviour
+{
+
+    public Sprite GreenButton;
+    public Sprite RedButton;
+    public Sprite PurpleButton;
+    public GameObject Human;
+    public Sprite GreenSprite;
+    public Sprite RedSprite;
+    public Sprite PurpleSprite;
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void OnMouseDown()
+    {
+        if (this.gameObject.GetComponent<SpriteRenderer>().sprite == GreenButton)
+        {
+            Human.gameObject.GetComponent<SpriteRenderer>().sprite = GreenSprite;
+        }
+        if (this.gameObject.GetComponent<SpriteRenderer>().sprite == RedButton)
+        {
+            Human.gameObject.GetComponent<SpriteRenderer>().sprite = RedSprite;
+        }
+        if (this.gameObject.GetComponent<SpriteRenderer>().sprite == PurpleButton)
+        {
+            Human.gameObject.GetComponent<SpriteRenderer>().sprite = PurpleSprite;
+        }
+    }
+}
