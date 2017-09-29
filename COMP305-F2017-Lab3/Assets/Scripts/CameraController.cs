30f112ab57cded71c9d5c060c76ee51aa10a24d0
deruip <aronly1997@hotmail.com>
2017-09-29 09:08:21 -0400

    Created Lab3

---
+++ COMP305-F2017-Lab3/Assets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    private Transform camTrans;
+    private UnityEngine.Camera camSize;
+    public Transform player;
+
+    // Use this for initialization
+    void Start()
+    {
+        camTrans = this.transform;
+        camSize = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.position.x > -14 && player.position.x < 15.45)
+        {
+            camTrans.position = new Vector3(player.position.x, player.position.y, camTrans.position.z);
+            camSize.orthographicSize = 5.0f;
+        }
+        else if (player.position.x >= 15.45 && player.position.x < 53.8)
+        {
+            camTrans.position = new Vector3(35f, 3.4f, camTrans.position.z);
+            camSize.orthographicSize = 9.5f;
+        }
+        else if (player.position.x >= 53.8)
+        {
+            camTrans.position = new Vector3(player.position.x, player.position.y, camTrans.position.z);
+            camSize.orthographicSize = Mathf.Clamp(90 - player.position.x, 2, 35);
+        }
+    }
+}
---
+++ COMP305-F2017-Lab3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    // Use this for physics
+    void FixedUpdate()
+    {
+        float hMove = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(hMove, 0, 0);
+        rb.velocity = movement * speed;
+    }
+}
